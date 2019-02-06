ad8fce1c60710d964d777d1eba3d6b0d52185300
Sunhyun Miller <sunhyunmiller@zipcoders-MBP.fios-router.home>
2019-02-06 06:46:38 -0500

    I created a method that takes an integer as input from the user, and returns the sum of 1 to that integer

--- Main.java
+++ Main.java
@@ -1,9 +1,30 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
-
+        int sum = sumOfInput();
+        System.out.print("The sum of one to your integer is: " + sum);
+    }
+    
+    /**
+     * This method takes an integer as input from the user, and
+     * returns the sum of 1 to that integer.
+     */
+    public static int sumOfInput(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+        int sum = 0;
+        
+        for (int i = 1; i <= number; i++){
+            sum = sum + i;
+        }
+        
+        return sum;
     }
 }
